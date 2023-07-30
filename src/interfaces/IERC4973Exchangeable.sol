71871028d616948ce5fe78fd6e061ba282968ca8
Felix Nordén <felixnorden@gmail.com>
2023-07-30 13:29:27 +0100

    chore: formatting

--- src/interfaces/IERC4973Exchangeable.sol
+++ src/interfaces/IERC4973Exchangeable.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.6;
 import {IERC4973Giveable} from "./IERC4973Giveable.sol";
 import {IERC4973Takeable} from "./IERC4973Takeable.sol";
 
-/** @title Exchangeable account-bound tokens
+/**
+ * @title Exchangeable account-bound tokens
  *  @dev See https://eips.ethereum.org/EIPS/eip-4973
  *  Note: the ERC-165 identifier for this interface is TODO: identifier
  */
